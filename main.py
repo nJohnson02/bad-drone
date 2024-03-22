baad7568da16a1c7d392ff2518efe480bcffa412
Nathan Johnson <55905788+nJohnson02@users.noreply.github.com>
2024-03-21 23:29:23 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from djitellopy import Tello
+from multiprocessing import Process
+import cv2
+import time
+
+def video_stream(tello):
+
+    out = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30,(648,488))
+
+    while True:
+        img = tello.get_frame_read().frame
+        img = cv2.resize(img, (648,488))
+        out.write(img)
+        cv2.imshow("Image",img)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            tello.streamoff()
+            break
+
+    out.release()
+
+def fly(tello):
+    tello.takeoff()
+    tello.move_forward(100)
+    tello.rotate_clockwise(90)
+    tello.move_forward(100)
+    tello.rotate_clockwise(90)
+    tello.move_forward(100)
+    tello.rotate_clockwise(90)
+    tello.move_forward(100)
+    tello.rotate_clockwise(90)
+    tello.land()
+
+if __name__ == '__main__':
+    
+    tello = Tello()
+    tello.connect()
+    print(tello.get_battery())
+
+    tello.streamoff()
+    tello.streamon()
+    Process(target=video_stream, args=(tello,)).start()
+    
+    time.sleep(10)
+    fly(tello)
